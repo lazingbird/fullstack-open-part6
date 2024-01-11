617630ac24a590b77a99f2f9c5d924177964168c
lazingbird <lazingbird@protonmail.com>
2024-01-11 15:15:09 -0300

    complete exercises 6.1 and 6.2

---
+++ unicafe/src/main.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import { createStore } from 'redux'
+import reducer from './reducer'
+
+const store = createStore(reducer)
+
+const App = () => {
+  const good = () => {
+    store.dispatch({
+      type: 'GOOD'
+    })
+  }
+  const handleCounters = (type) => {
+    store.dispatch({
+      type: type
+    })
+  }
+
+  return (
+    <div>
+      <button onClick={() => handleCounters("GOOD")}>good</button> 
+      <button onClick={() => handleCounters("OK")}>ok</button> 
+      <button onClick={() => handleCounters("BAD")}>bad</button>
+      <button onClick={() => handleCounters("ZERO")}>reset stats</button>
+      <div>good {store.getState().good}</div>
+      <div>ok {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const renderApp = () => {
+  root.render(<App />)
+}
+
+renderApp()
+store.subscribe(renderApp)
